22fc97fcbc47f5c9c8ca902ce58c1182f3fd3312
Stanislav Bzhezinskyi <stasiva28@gmail.com>
2022-08-04 14:53:59 +0300

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,46 @@
 'use strict';
 
-// write your code here
+const listURL = 'https://mate-academy.github.io/phone-catalogue-static/api';
+const list = document.createElement('ul');
+
+const request = (url) => {
+  return fetch(`${listURL}${url}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} --- ${response.statusText}`);
+      }
+
+      return response.json();
+    });
+};
+
+const getPhones = () => request(`/phones.json`);
+
+const getPhonesDetails = (phones) => {
+  const phonesWithDetails = [];
+
+  phones.map(phone => {
+    request(`/phones/${phone.id}.json`)
+      .then(response => {
+        list.insertAdjacentHTML('beforeend', `
+          <li>${response.name}</li>
+        `);
+
+        phonesWithDetails.push(
+          {
+            ...phone,
+            details: response,
+          }
+        );
+      });
+  });
+  document.body.append(list);
+
+  return phonesWithDetails;
+};
+
+getPhones()
+  .then(result => getPhonesDetails(result))
+  .catch(error => {
+    setTimeout(() => document.body.append(error), 5000);
+  });
